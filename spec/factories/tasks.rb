33763a543c94c8b2f2acf1bc927012139f02ceec
yasumichitake <t.yasumichi0515@gmail.com>
2021-01-14 00:52:49 +0900

    --format documentation

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :task do
-    
+    association :user
+    title { "title" }
+    content { "content" }
+    status { 0 }
   end
 end
+
+# FactoruBotでの定義づけは{}ブロックで囲む必要がある
