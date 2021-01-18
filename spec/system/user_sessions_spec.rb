f02544dd8e14a2983dac2bf3f9848941ee9fd1f6
yasumichitake <t.yasumichi0515@gmail.com>
2021-01-18 23:39:13 +0900

    [ADD] テストケース構成追加

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "UserSessions", type: :system do
-  before do
-    driven_by(:rack_test)
+  describe 'ログイン前' do
+    context 'フォームの入力値が正常' do
+      it 'ログイン処理が成功する'
+    end
+    context 'フォームが未入力' do
+      it 'ログイン処理が失敗する'
+    end
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe 'ログイン後' do
+    context 'ログアウトボタンをクリック' do
+      it 'ログアウト処理が成功する'
+    end
+  end
 end
