2d53f0f20d687f9dfa7e6fe4ddfd59d423270298
yasumichitake <t.yasumichi0515@gmail.com>
2021-01-31 16:17:27 +0900

    回答確認後修正

--- login_macros.rb
+++
@@ -1,9 +0,0 @@
-module LoginMacros
-
-  def sign_in(user)
-    visit login_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Login'
-  end
-end
--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,26 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe "UserSessions", type: :system do
-  before do
-    User.create!(email: 'aaa@example.com', password: '123456', password_confirmation: '123456')
-  end
+RSpec.describe 'UserSessions', type: :system do
+  let(:user) { create(:user) }
+
   describe 'ログイン前' do
     context 'フォームの入力値が正常' do
       it 'ログイン処理が成功する' do
         visit login_path
-        fill_in 'Email', with: 'aaa@example.com'
-        fill_in 'Password', with: '123456'
+        fill_in 'Email', with: user.email
+        fill_in 'Password', with: 'password'
         click_button 'Login'
         expect(page).to have_content 'Login successful'
         expect(current_path).to eq root_path
-
       end
     end
+
     context 'フォームが未入力' do
       it 'ログイン処理が失敗する' do
         visit login_path
         fill_in 'Email', with: ''
-        fill_in 'Password', with: ''
+        fill_in 'Password', with: 'password'
         click_button 'Login'
         expect(page).to have_content 'Login failed'
         expect(current_path).to eq login_path
@@ -31,13 +30,7 @@ RSpec.describe "UserSessions", type: :system do
   describe 'ログイン後' do
     context 'ログアウトボタンをクリック' do
       it 'ログアウト処理が成功する' do
-        visit login_path
-        fill_in 'Email', with: 'aaa@example.com'
-        fill_in 'Password', with: '123456'
-        click_button 'Login'
-        expect(page).to have_content 'Login successful'
-        expect(current_path).to eq root_path
-
+        login_as(user)
         click_link 'Logout'
         expect(page).to have_content 'Logged out'
         expect(current_path).to eq root_path
